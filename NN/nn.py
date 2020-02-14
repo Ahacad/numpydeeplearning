d54053e5db92a077e2cb8240de85a3185af7b91a
Ahacad <51874356+Ahacad@users.noreply.github.com>
2020-02-14 14:03:16 +0800

    Added nn.py

---
+++ NN/nn.py
@@ -0,0 +1,12 @@
+Object nn:
+    
+    def __init__(self, layers):
+        self.layers = layers
+        self.layerNumber = len(layers)
+        
+    def run():
+        pass 
+     
+    def train():
+        pass
+        
