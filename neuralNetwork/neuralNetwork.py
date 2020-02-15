8f2ded16042a0f316986ec79ca4ca6f970eb6195
Ahacad <1393345075@qq.com>
2020-02-15 08:02:06 +0800

    testing neuralNetwork :bug:

--- neuralNetwork/neuralNetwork.py
+++ neuralNetwork/neuralNetwork.py
@@ -1,18 +1,23 @@
 import numpy as np
+import os 
+import struct
 
 class neuralNetwork:
 
-    def __init__(self, inNodes, hiddenNodes, outNodes, lr, activation='sigmodi'
+    def __init__(self, inNodes, hiddenNodes, outNodes, lr, activation='sigmoid'):
         self.inNodes = inNodes
         self.hiddenNodes = hiddenNodes
         self.outNodes = outNodes
         self.lr = lr
         self.wih = np.random.normal(0.0, pow(self.hiddenNodes, -0.5), (self.hiddenNodes, self.inNodes))
         self.who = np.random.normal(0.0, pow(self.outNodes, -0.5), (self.outNodes, self.hiddenNodes))
+        def SIGMOID(x):
+            return 1/(1+np.exp(-x))
+
         if activation == 'sigmoid':
-            self.activation = lambda x: x[x<=0] = 0
+            self.activation = lambda x: SIGMOID
         else:
-            self.activation = lambda x: 1/(1+np.exp(-x))
+            self.activation = lambda x:x
         
     def train(self, inputVector,labels):
         inputs = inputVector.reshape(-1,1)
@@ -33,3 +38,37 @@ class neuralNetwork:
         finalInputs = np.dot(self.who, hiddenOutputs)
         finalOutputs = self.activation(finalInputs)
         
+        return finalOutputs
+
+        
+def loadMnist(path, kind='train'):
+    '''
+    import the MNIST dataset from path, which is the path of the folder
+    kind should be either 'train', which is the training set or 't10k', meaning test-10k pictures
+    '''
+    imagePath = os.path.join(path, '%s-images.idx3-ubyte' % kind)
+    labelPath = os.path.join(path, '%s-labels.idx1-ubyte' % kind)
+
+    with open(labelPath, 'rb') as lbp:
+        magic, n = struct.unpack('>II', lbp.read(8))
+        label = np.fromfile(lbp, dtype=np.uint8)
+
+    with open(imagePath, 'rb') as imp:
+        magic, num, rows, columns = struct.unpack('>IIII', imp.read(16))
+        image = np.fromfile(imp, dtype=np.uint8)
+        image = image.reshape(len(label),784)
+
+    return image, label, 
+    
+trainSet, trainLabels = loadMnist('MNIST', 'train')
+test, testLabels = loadMnist('MNIST', 't10k')
+nn = neuralNetwork(trainSet[0].shape[0], 1000, 12, 1)
+for i in range(1000):
+    nn.train(trainSet[i], trainLabels[i])
+
+for i in range(300):
+    outputArg = np.argmax(nn.forward(test[i]).reshape(-1))
+    right = 0
+    if outputArg == trainLabels[i]:
+        right += 1
+print('accuracy = %f' % (right/30))
